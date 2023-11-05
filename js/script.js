db999105edba03b7cb6f6e405a76d9fe6256d2c1
Silvan Stuber <silvan.stuber93@gmail.com>
2023-11-05 22:56:15 +0100

    Colored feedback for incorrect or correct answers

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ function init() {
 }
 
 function startQuiz(i) {
-  let questions  = i;  
-  document.getElementById('content').innerHTML = generaterQuizContent(questions);         
+  let questions = i;
+  document.getElementById('content').innerHTML = generaterQuizContent(questions);
   document.getElementById('all-questions').innerHTML = questions.length - 1;
   showQuestion(questions);
   whichQuiz = [];
@@ -15,27 +15,27 @@ function startQuiz(i) {
 
 function showQuestion(questions) {
   let question = whichQuestion(questions);
-  document.getElementById('questiontext').innerHTML = question['question']; 
-  document.getElementById('answer1').innerHTML = question['answer_1'];
-  document.getElementById('answer2').innerHTML = question['answer_2'];
-  document.getElementById('answer3').innerHTML = question['answer_3'];
-  document.getElementById('answer4').innerHTML = question['answer_4'];
+  document.getElementById('questiontext').innerHTML = question['question'];
+  document.getElementById('answer_1').innerHTML = question['answer_1'];
+  document.getElementById('answer_2').innerHTML = question['answer_2'];
+  document.getElementById('answer_3').innerHTML = question['answer_3'];
+  document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
 
 function whichQuestion(questions) {
   let valueFromQuestion = questions[0]['currentQuestion'];
   let question = questions[valueFromQuestion];
-  return(question);
+  return (question);
 }
 
 function answer(selection) {
   let questions = whichQuiz;
   let question = whichQuestion(questions[0]);
-  let selectedQuestionNumber = selection.slice(-1); //just the last letter from the variable
+  let selectedQuestionNumber = selection.slice(-1);
   if (selectedQuestionNumber == question['right_answer']) {
-    console.log('Richtige Antwort')
+    document.getElementById(selection).parentNode.classList.add('bg-success');
   } else {
-    console.log('Falsche Antwort')
+    document.getElementById(selection).parentNode.classList.add('bg-danger');
   }
 }
 
@@ -43,13 +43,13 @@ function save() {
   let whichQuizAtText = JSON.stringify(whichQuiz);
   localStorage.setItem('whichquiz', whichQuizAtText);
 }
+
 function load() {
   let whichQuizAtText = localStorage.getItem('whichquiz');
   if (whichQuizAtText) {
-    whichQuiz = JSON.parse(whichQuizAtText); 
+    whichQuiz = JSON.parse(whichQuizAtText);
   }
 }
-
 
 function generateHomePage() {
   return /*html*/ `
@@ -60,48 +60,44 @@ function generateHomePage() {
 function generaterQuizContent() {
   return /*html*/ `
   <div class="card-body answer-container">
-              <h5 class="card-title p-3" id="questiontext"></h5>
-              <div class="card mt-4 answer-card">
-                <div onclick="answer('answer_1')" class="card-body answer-content">
-                  <div class="letter-card">
-                    <b>A</b>
-                  </div>
-                  <div>
-                    <b id="answer1"></b>
-                  </div>
-                </div>
-              </div>
-              <div onclick="answer('answer_2')" class="card mt-4 answer-card">
-                <div class="card-body answer-content">
-                  <div class="letter-card">
-                    <b>B</b>
-                  </div>
-                  <b id="answer2"></b>
-                </div>
-              </div>
-              <div onclick="answer('answer_3')" class="card mt-4 answer-card">
-                <div class="card-body answer-content">
-                  <div class="letter-card">
-                    <b>C</b>
-                  </div>
-                  <b id="answer3"></b>
-                </div>
-              </div>
-              <div onclick="answer('answer_4')" class="card mt-4 answer-card">
-                <div class="card-body answer-content">
-                  <div class="letter-card">
-                    <b>D</b>
-                  </div>
-                  <b id="answer4"></b>
-                </div>
-              </div>
-            </div>
-            <div class="footer-card">
-              <img class="arrow" src="./img/arrowleft.png" alt="arrowleft." />
-              <div>
-                <b>1</b> von <b id="all-questions">5</b> Fragen
-              </div>
-              <img class="arrow" src="./img/arrowright.png" alt="arrowright" />
-            </div>
+    <h5 class="card-title p-3" id="questiontext"></h5>
+    <div onclick="answer('answer_1')" class="card mt-4 answer-card">
+      <div class="card-body answer-content">
+        <div class="letter-card">
+          <b>A</b>
+        </div>
+        <b id="answer_1"></b>
+      </div>
+    </div>
+    <div onclick="answer('answer_2')" class="card mt-4 answer-card">
+      <div class="card-body answer-content">
+        <div class="letter-card">
+          <b>B</b>
+        </div>
+        <b id="answer_2"></b>
+      </div>
+    </div>
+    <div onclick="answer('answer_3')" class="card mt-4 answer-card">
+      <div class="card-body answer-content">
+        <div class="letter-card">
+          <b>C</b>
+        </div>
+        <b id="answer_3"></b>
+      </div>
+    </div>
+    <div onclick="answer('answer_4')" class="card mt-4 answer-card">
+      <div class="card-body answer-content">
+        <div class="letter-card">
+          <b>D</b>
+        </div>
+        <b id="answer_4"></b>
+      </div>
+    </div>
+  </div>
+<div class="footer-card">
+  <img class="arrow" src="./img/arrowleft.png" alt="arrowleft." />
+  <div><b>1</b> von <b id="all-questions">5</b> Fragen</div>
+  <img class="arrow" src="./img/arrowright.png" alt="arrowright" />
+</div>
   `;
 }
